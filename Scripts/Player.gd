0093a350ef2548dd5c42f2c1ac1ee1b5743ef3c3
Fabian Kalweit <fabiankalweit@yahoo.de>
2020-11-26 21:22:50 +0100

    refactor movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-var movespeed = 10
+var movespeed = 400
 
-var playerPosition = Vector2()
+
 
 enum MoveDirection { UP, DOWN, LEFT, RIGHT, NONE}
 
@@ -13,43 +13,54 @@ slave var slave_movement = MoveDirection.NONE
 func _ready():
 	pass # Replace with function body.
 
-func _physics_process(delta):
-	var direction = MoveDirection.NONE
+
+func _physics_process (delta):
+	var direction = []
 	if is_network_master():	
 		if Input.is_action_pressed("player_up"):
-			direction = MoveDirection.UP
+			direction.append(MoveDirection.UP) 		
 		if Input.is_action_pressed("player_down"):
-			direction = MoveDirection.DOWN
+			direction.append(MoveDirection.DOWN) 	
 		if Input.is_action_pressed("player_left"):
-			direction = MoveDirection.LEFT
+			direction.append(MoveDirection.LEFT) 	
 		if Input.is_action_pressed("player_right"):
-			direction = MoveDirection.RIGHT
+			direction.append(MoveDirection.RIGHT) 	
 		
 		rset_unreliable('slave_position', position)
 		rset_unreliable('slave_movement', direction)
-		_move(direction)
+		_move(direction, delta)
 	
 	else:
-		_move(slave_movement)
+		_move(slave_movement, delta)
 		position = slave_position
 		
 	if get_tree().is_network_server():
-		Network.update_position(int(name), playerPosition)
+		Network.update_position(int(name), position)
 		
-func _move(direction):
-	match direction: 
-		MoveDirection.NONE: 
-			return
-		MoveDirection.UP:
-			move_and_collide(Vector2(0, -movespeed))
-		MoveDirection.DOWN:
-			move_and_collide(Vector2(0, movespeed))
-		MoveDirection.LEFT:
-			move_and_collide(Vector2(-movespeed, 0 ))
-		MoveDirection.RIGHT:
-			move_and_collide(Vector2(movespeed, 0 ))
+func _move(direction, delta):
+	print(direction)
+	print(delta)
+	var velocity = Vector2()
+	if not direction:
+		return
+	if MoveDirection.RIGHT in direction:
+		velocity.x += 1
+	if  MoveDirection.LEFT in direction:
+		velocity.x -= 1
+	if MoveDirection.DOWN in direction:
+		velocity.y += 1
+	if MoveDirection.UP in direction:
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * movespeed
+
+	position += velocity * delta
+
+
+
 		
 func init(name, position, is_slave):
-	playerPosition = position
+	position = position
 	if is_slave:
 		$Sprite.texture = load('res://Resources/Player.png')
